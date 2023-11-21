1ecc0cd0c2060b0dd7f95fded89979a5d7b60188
Phiranath PO-NGERN <phukaow18@gmail.com>
2023-11-21 23:41:17 +0700

    changed a little

--- database.py
+++ database.py
@@ -5,15 +5,15 @@ import csv
 import os
 import copy
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
 
 class Read:
     def __init__(self, name):
         self.name = name
 
     def readCSV(self):
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__))
+        )
         _list = []
         with open(os.path.join(__location__, self.name)) as f:
             rows = csv.DictReader(f)
@@ -21,24 +21,12 @@ class Read:
                 _list.append(dict(row))
         return _list
 
-
-def gen_comb_list(list_set):
-    if len(list_set) == 1:
-        start_list = []
-        for item in list_set[0]:
-            start_list.append([item])
-        return start_list
-    comb_list_temp = gen_comb_list(list_set[0:-1])
-    start_list = []
-    for list_item in comb_list_temp:
-        for val in list_set[-1]:
-            temp_item = copy.deepcopy(list_item)
-            temp_item.append(val)
-            start_list.append(temp_item)
-    return start_list
+    def __str__(self):
+        return f"Data: {self.readCSV()}"
 
 
 # add in code for a Database class
+
 
 class DB:
     def __init__(self):
@@ -54,8 +42,14 @@ class DB:
         print(f"No table name {name}. Please try again")
         return None
 
+    def delete(self, table_delete):
+        if self.search(table_delete) is not None:
+            self.database.remove(table_delete)
+        self.search(table_delete)
+
 
 # add in code for a Table class
+
 
 class Table:
     def __init__(self, name, table):
@@ -77,7 +71,7 @@ class Table:
         return joined_table
 
     def filter(self, condition):
-        filtered_table = Table(self.name + '_filtered', [])
+        filtered_table = Table(self.name + "_filtered", [])
         for item1 in self.table:
             if condition(item1):
                 filtered_table.table.append(item1)
@@ -111,44 +105,17 @@ class Table:
             temps.append(dict_temp)
         return temps
 
-    def pivot_table(self, keys_to_pivot_list, keys_to_aggreagte_list,
-                    aggregate_func_list):
-
-        unique_values_list = []
-        for key_item in keys_to_pivot_list:
-            temp = []
-            for _dict in self.table:
-                if _dict[key_item] not in temp:
-                    temp.append(_dict[key_item])
-            unique_values_list.append(temp)
-
-        # combination of unique value lists
-        comb_list = gen_comb_list(unique_values_list)
-
-        pivot_table = []
-        # filter each combination
-        for item in comb_list:
-            temp_filter_table = self
-            for i in range(len(item)):
-                temp_filter_table = temp_filter_table.filter(
-                    lambda x: x[keys_to_pivot_list[i]] == item[i])
-
-            # aggregate over the filtered table
-            aggregate_val_list = []
-            for i in range(len(keys_to_aggreagte_list)):
-                aggregate_val = temp_filter_table.aggregate(
-                    aggregate_func_list[i], keys_to_aggreagte_list[i])
-                aggregate_val_list.append(aggregate_val)
-            pivot_table.append([item, aggregate_val_list])
-        return pivot_table
-
     def __str__(self):
-        return self.name + ':' + str(self.table)
+        return self.name + ":" + str(self.table)
 
 
 # modify the code in the Table class so that it supports the insert operation
 # where an entry can be added to a list of dictionary
 
+
 def test():
-    persons = Read('persons.csv')
-    print(persons.readCSV())
+    persons = Read("persons.csv")
+    print(persons)
+
+
+test()
